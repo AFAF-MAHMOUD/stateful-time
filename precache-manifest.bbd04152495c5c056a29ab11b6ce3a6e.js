2b0c9d8b09581e5fe5668891695160a63e9d00aa
AFAF-MAHMOUD <afefmahmoud21031985@yahoo.ca>
2019-12-02 11:11:28 +0100

    Updates

---
+++ precache-manifest.bbd04152495c5c056a29ab11b6ce3a6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a64d5919a9bcb4f113e046a0eb38aa9",
+    "url": "/stateful-time/index.html"
+  },
+  {
+    "revision": "3a08b9b6391cc7d602fd",
+    "url": "/stateful-time/static/css/main.1ffd173b.chunk.css"
+  },
+  {
+    "revision": "a779e2f6f691021a1382",
+    "url": "/stateful-time/static/js/2.339231ef.chunk.js"
+  },
+  {
+    "revision": "3a08b9b6391cc7d602fd",
+    "url": "/stateful-time/static/js/main.cd1f6587.chunk.js"
+  },
+  {
+    "revision": "42ca53e75d8b8f12fe3b",
+    "url": "/stateful-time/static/js/runtime-main.da8fcf0f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/stateful-time/precache-manifest.bbd04152495c5c056a29ab11b6ce3a6e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stateful-time/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
